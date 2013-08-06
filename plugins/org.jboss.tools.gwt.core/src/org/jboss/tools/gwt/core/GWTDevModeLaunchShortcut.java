76086d0bbaf3e9c48ece3fd39f25c665f1004cd4
Daniel Azarov <daniel@exadel.com>
2013-08-06 10:48:53 -0700

    JBIDE-13748 Create a Run As... GWT Development Mode

--- plugins/org.jboss.tools.gwt.core/src/org/jboss/tools/gwt/core/GWTDevModeLaunchShortcut.java
+++ plugins/org.jboss.tools.gwt.core/src/org/jboss/tools/gwt/core/GWTDevModeLaunchShortcut.java
@@ -1,5 +1,7 @@
 package org.jboss.tools.gwt.core;
 
+import java.util.ArrayList;
+
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -17,6 +19,9 @@ import org.eclipse.jdt.debug.ui.launchConfigurations.JavaApplicationLaunchShortc
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jdt.internal.debug.ui.launcher.LauncherMessages;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
+import org.eclipse.jdt.launching.IRuntimeClasspathProvider;
+import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.jboss.tools.common.EclipseUtil;
@@ -30,17 +35,30 @@ public class GWTDevModeLaunchShortcut extends JavaApplicationLaunchShortcut {
 		for(Object element : elements){
 			if(element instanceof IProject){
 				IJavaProject javaProject = EclipseUtil.getJavaProject((IProject)element);
-				if(javaProject != null){
-					try {
-						IType type = javaProject.findType("com.google.gwt.dev.DevMode");
-						return new IType[]{type};
-					} catch (JavaModelException e) {
-						e.printStackTrace();
-					}
+				IType type = findType(javaProject);
+				if(type != null){
+					return new IType[]{type};
 				}
+			}else if(element instanceof IJavaProject){
+				IType type = findType((IJavaProject)element);
+				if(type != null){
+					return new IType[]{type};
+				}
+				
 			}
 		}
 		return new IType[]{};
+	}
+	
+	private IType findType(IJavaProject javaProject){
+		if(javaProject != null){
+			try {
+				return javaProject.findType("com.google.gwt.dev.DevMode");
+			} catch (JavaModelException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 	
 	private String findModule(IProject project){
@@ -87,23 +105,53 @@ public class GWTDevModeLaunchShortcut extends JavaApplicationLaunchShortcut {
 	
 	@Override
 	protected ILaunchConfiguration createConfiguration(IType type) {
+		IJavaProject javaProject = type.getJavaProject();
+		IProject project = javaProject.getProject();
+		
+		String moduleName = findModule(project);
+		
 		ILaunchConfiguration config = null;
 		ILaunchConfigurationWorkingCopy wc = null;
 		try {
 			ILaunchConfigurationType configType = getConfigurationType();
-			wc = configType.newInstance(null, DebugPlugin.getDefault().getLaunchManager().generateLaunchConfigurationName(type.getTypeQualifiedName('.')));
+			wc = configType.newInstance(null, DebugPlugin.getDefault().getLaunchManager().generateLaunchConfigurationName(type.getTypeQualifiedName('.'))+" "+project.getName()+" "+moduleName);
+			
+			IRuntimeClasspathProvider provider = JavaRuntime.getClasspathProvider(wc);
+			
+			ArrayList<String> paths = new ArrayList<String>();
+			IRuntimeClasspathEntry[] entries = provider.computeUnresolvedClasspath(wc);
+			for(IRuntimeClasspathEntry entry : entries){
+				paths.add(entry.getMemento());
+			}
+			
+			// add source folder to the class path
+			for(IResource resource : EclipseUtil.getJavaSourceRoots(project)){
+				IRuntimeClasspathEntry entry = JavaRuntime.newArchiveRuntimeClasspathEntry(resource.getFullPath());
+				paths.add(entry.getMemento());
+			}
+
+			// add default project class path
+			paths.add(JavaRuntime.newDefaultProjectClasspathEntry(javaProject).getMemento());
+			
+			paths.add(JavaRuntime.newArchiveRuntimeClasspathEntry(type.getPath()).getMemento());
+			
+			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_DEFAULT_CLASSPATH, false);
+			
+			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH, paths);
+			
 			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, type.getFullyQualifiedName());
 			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, type.getJavaProject().getElementName());
 			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, "-Xmx256M");
 			
-			String moduleName = findModule(type.getJavaProject().getProject());
-			
-			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, "-startupUrl index.html -war src/main/webapp"+" "+moduleName);
+			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, 
+					"-startupUrl index.html -war src/main/webapp -codeServerPort 9997 -port 8888 -remoteUI \"${gwt_remote_ui_server_port}:${unique_id}\" "
+					+moduleName);
 			
 			wc.setMappedResources(new IResource[] {type.getUnderlyingResource()});
 			config = wc.doSave();
 		} catch (CoreException exception) {
-			MessageDialog.openError(JDIDebugUIPlugin.getActiveWorkbenchShell(), LauncherMessages.JavaLaunchShortcut_3, exception.getStatus().getMessage());	
+			MessageDialog.openError(JDIDebugUIPlugin.getActiveWorkbenchShell(), LauncherMessages.JavaLaunchShortcut_3, exception.getStatus().getMessage());
+			exception.printStackTrace();
 		} 
 		return config;
 	}
